94af3fc2d336441252284105e2e74ad8a44e88ba
NaitikPatel <np03022005@gmail.com>
2023-05-25 13:18:25 +0530

    for pagination

--- attKris/admin.php
+++ attKris/admin.php
@@ -11,24 +11,36 @@
   <script src="script.js" defer></script>
 </head>
 <body>
-<!-- changed by naitk  3-->
-<div id="main-content" >
+
+<div id="main-content">
     <h2>All Records</h2>
     <?php
-        $conn = mysqli_connect("localhost","root","","ce") or die("connection Failed");
-        $sql= "SELECT * FROM students";
+        session_start();    
+        // $conn = mysqli_connect("localhost","root","","ce") or die("connection Failed");
+        require 'config.php';
+        $limit = 26;
+        if(isset($_GET['page']))
+        {
+            $page = $_GET['page'];
+        }
+        else{
+            $page = 1;
+        }
+        $offset = ($page - 1)*$limit;
+
+        $sql= "SELECT * FROM `students` LIMIT {$offset},{$limit}";
         $result = mysqli_query($conn,$sql) or die("Query Unsucessful");
         if(mysqli_num_rows($result)>0){
-    ?>
+        ?>
     <table cellpadding="10px">
         <thead>
             <th>Roll No</th>
             <th>Name</th>
             <th>ID</th>
-            <th>Enrollment Year</th>
-            <th>Batch Year</th>
-            <th>Session No</th>
-            <th>Gender</th>
+            <!-- <th>Enrollment Year</th> -->
+            <!-- <th>Batch Year</th> -->
+            <!-- <th>Session No</th> -->
+            <!-- <th>Gender</th> -->
             <th>Division</th>
         </thead>
         <tbody>
@@ -36,23 +48,43 @@
             while($row = mysqli_fetch_assoc($result)){
             ?>
             <tr>
-                <td><?php echo $row['Roll No.'] ?></td>
-                <td><?php echo $row['ID'] ?></td>
-                <td><?php echo $row['Name'] ?></td>
-                <td><?php echo $row['Enrollment Year'] ?></td>
-                <td><?php echo $row['Batch Year'] ?></td>
-                <td><?php echo $row['Session No'] ?></td>
-                <td><?php echo $row['Gender'] ?></td>
-                <td><?php echo $row['Division'] ?></td>
+                <td><?php echo $row['Roll No.']?></td>
+                <td><?php echo $row['ID']?></td>
+                <td><?php echo $row['Name']?></td>
+                <!-- <td><?php echo $row['Enrollment Year']?></td> -->
+                <!-- <td><?php echo $row['Batch Year']?></td> -->
+                <!-- <td><?php echo $row['Session No']?></td> -->
+                <!-- <td><?php echo $row['Gender']?></td> -->
+                <td><?php echo $row['Division']?></td>
                 <td>
-                    <a href='edit.php?id=<?php echo $row['ID'] ?>'>Edit</a>
+                    <a href='edit.php?id=<?php echo $row['ID']?>'>Edit</a>
                     <a href='delete.php?id=<?php echo $row['ID'] ?>' onclick="return confirmDelete('<?php echo $row['ID'] ?>')">Delete</a>
                 </td>
             </tr>
             <?php } ?>
         </tbody>
     </table>
-    <?php } ?>
+    <?php } 
+    $sql1= "SELECT * FROM `students`";
+    $result1 = mysqli_query($conn,$sql1) or die("Query Unsucessful");
+    if(mysqli_num_rows($result1) > 0){
+        $total_records =  mysqli_num_rows($result1);
+        $total_pages = ceil($total_records/$limit);
+        echo '<ul class = "pagination admin-pagination">';
+        for($i = 1; $i <= $total_pages;$i++)
+        {
+            if($i== $page)
+            {
+                $active = "active";
+            }
+            else{
+                $active = "";
+            }
+            echo '<li class="'.$active.'"><a href="admin.php?page='.$i.'">'.$i.'</a></li>';
+        }
+        echo '</ul>';
+    }
+    ?>
 </div>
 </div>
 </body>
--- attKris/hw1project/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $id=$_GET['id'];
-    $conn = mysqli_connect("localhost","root","","ce") or die("connection Failed");
-    $query = "DELETE FROM students WHERE `ID` = '$id'";
-    $result = mysqli_query($conn, $query);
-    header("Location:admin.php");
-    exit();
-?> 
