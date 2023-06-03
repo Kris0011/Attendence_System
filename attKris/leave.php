f864335c5f4c1283fee47da568bbd22661628f59
dp3903 <122767649+dp3903@users.noreply.github.com>
2023-06-03 11:41:05 +0530

    Update leave.php

--- attKris/leave.php
+++ attKris/leave.php
@@ -5,25 +5,26 @@
     if (isset($_POST['date1'])) {
         $startingdate = $_POST['date1'];
     } else {
-        echo "something error occured";
+        echo "<h1>starting date not set.</h1>";
     }
     if (isset($_POST['date2'])) {
         $endingdate = $_POST['date2'];
     } 
     else 
     {
-        echo "something error occured";
+        echo "<h1>ending date not set.</h1>";
     }    
     $email = $_POST['email'];
     $subject = "About Leaving";
     $message = $_POST['m'];
     $to="np03022005@gmail.com";
     $headers = "From: $email";
+    // set_ini("SMTP")
     if(mail($to,$subject,$message,$headers)){
-        echo "mail sent";
+        echo "<h1>mail sent successfuly</h1>";
     }
     else{
-        echo "mail error occured";
+        echo "<h1>mail error occured</h1>";
     }
     // echo $email;
     // echo $endingdate ;
